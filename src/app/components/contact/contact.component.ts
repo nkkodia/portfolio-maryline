f20837d40bd5c41f5ecac651c8179f3ed37a7109
Maryline Kouamé-Kodia <maryline.kouame-kodia@koralplay.com>
2026-01-14 08:56:02 +0100

    validate form

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,33 +1,54 @@
 import {Component} from '@angular/core';
 import emailjs from '@emailjs/browser';
 import {Router} from '@angular/router';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
+  imports: [ReactiveFormsModule, CommonModule], // Assure-toi d'avoir ReactiveFormsModule
   templateUrl: './contact.component.html'
 })
 export class ContactComponent {
-
-  constructor(private router: Router) {}
+  contactForm: FormGroup;
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required], // Ajout du nom car présent dans ton HTML
+      subject: ['', Validators.required],
+      contactMethod: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
 
   sendEmail() {
-    // On récupère les données
-    const templateParams = {
-      from_name: (document.getElementById('name') as HTMLInputElement).value,
-      contact_info: (document.getElementById('contact_info') as HTMLInputElement).value,
-      subject: (document.getElementById('subject') as HTMLInputElement).value,
-      message: (document.getElementById('message') as HTMLTextAreaElement).value
-    };
+    console.log("Tentative d'envoi...");
 
-    // Envoi via EmailJS (Remplace par tes clés)
-    emailjs.send('service_0zp4wf5', 'template_2dah04h', templateParams, '3YMl1r8RZqBgbPOxW')
-      .then(() => {
-        alert('Votre message est bien parti ! ✨');
-        this.router.navigate(['/']);
-      }, (error) => {
-        console.error('Erreur:', error);
-        alert('Petit souci technique... Vous pouvez m\'écrire à marykodia1045@gmail.com');
-      });
+    if (this.contactForm.valid) {
+      const values = this.contactForm.value;
+
+      const templateParams = {
+        from_name: values.name,
+        contact_info: values.contactMethod,
+        subject: values.subject,
+        message: values.message
+      };
+
+      emailjs.send(
+        'service_0zp4wf5',
+        'template_2dah04h',
+        templateParams,
+        '3YMl1r8RZqBgbPOxW'
+      )
+        .then(() => {
+          this.contactForm.reset(); // On vide le formulaire
+          this.router.navigate(['/']);
+        }, (error) => {
+          console.error('Erreur EmailJS:', error);
+          alert('Petit souci technique... Écrivez-moi à marykodia1045@gmail.com');
+        });
+    } else {
+      console.log("Le formulaire n'est pas rempli correctement.");
+    }
   }
 }
