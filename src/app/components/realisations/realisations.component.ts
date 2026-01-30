13b7ef5776ac069cb2dd86250ef514473e5a660f
Maryline Kouamé-Kodia <maryline.kouame-kodia@koralplay.com>
2026-01-30 16:06:45 +0100

    switch

--- src/app/components/realisations/realisations.component.ts
+++ src/app/components/realisations/realisations.component.ts
@@ -21,6 +21,14 @@ export class RealisationsComponent {
 
   projects: Project[] = [
     {
+      title: 'Janice Alleme-Yamthieu',
+      category: 'Bien-être & Méditation',
+      image: 'janice.png',
+      link: 'https://janiceallemeyamthieu.fr/meditations',
+      tech: 'WordPress / Premium Design',
+      desc: 'Création d’un univers zen et épuré pour une plateforme de méditations guidées. Optimisation du parcours utilisateur pour un accès fluide aux contenus audio.'
+    },
+    {
       title: 'Filadéo Corporate',
       category: 'Conseil & Stratégie',
       image: 'filadeo.png',
@@ -28,14 +36,7 @@ export class RealisationsComponent {
       tech: 'Wix Business / UX Design',
       desc: 'Conception d’une vitrine institutionnelle pour une structure de conseil, mettant l’accent sur le professionnalisme et la clarté de l’offre B2B.'
     },
-    {
-      title: 'Janice Alleme-Yamthieu',
-      category: 'Bien-être & Méditation',
-      image: 'janice.png',
-      link: 'https://janiceallemeyamthieu.fr/meditations',
-      tech: 'WordPress / Premium Design',
-      desc: 'Création d’un univers zen et épuré pour une plateforme de méditations guidées. Optimisation du parcours utilisateur pour un accès fluide aux contenus audio.'
-    }
+
   ];
 
   constructor() {}
