8bab09fed3a985fe279b56468e543839147c53f1
Maryline Kouamé-Kodia <maryline.kouame-kodia@koralplay.com>
2026-01-30 14:08:31 +0100

    add real

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import {ContactComponent} from './components/contact/contact.component';
 import {ServicesComponent} from './components/services/services.component';
+import {RealisationsComponent} from './components/realisations/realisations.component';
 
 export const routes: Routes = [
-  { path: '', component: AboutComponent },         // Page d'accueil
+  { path: '', component: AboutComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'contact', component: AboutComponent },   // On réutilise About en attendant
-  { path: '**', redirectTo: '' }                   // Redirection de sécurité
+  { path: 'contact', component: AboutComponent },
+  { path: 'realisations', component: RealisationsComponent },
+  { path: '**', redirectTo: '' }
 ];
---
+++ src/app/components/realisations/realisations.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+interface Project {
+  title: string;
+  category: string;
+  image: string;
+  link: string;
+  tech: string;
+  desc: string;
+}
+
+@Component({
+  selector: 'app-realisations',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './realisations.component.html'
+})
+export class RealisationsComponent {
+
+  projects: Project[] = [
+    {
+      title: 'Filadéo Corporate',
+      category: 'Conseil & Stratégie',
+      image: 'filadeo.png',
+      link: 'https://janyslaurealleme.wixsite.com/filadeocorporate/professionnels',
+      tech: 'Wix Business / UX Design',
+      desc: 'Conception d’une vitrine institutionnelle pour une structure de conseil, mettant l’accent sur le professionnalisme et la clarté de l’offre B2B.'
+    },
+    {
+      title: 'Janice Alleme-Yamthieu',
+      category: 'Bien-être & Méditation',
+      image: 'janice.png',
+      link: 'https://janiceallemeyamthieu.fr/meditations',
+      tech: 'WordPress / Premium Design',
+      desc: 'Création d’un univers zen et épuré pour une plateforme de méditations guidées. Optimisation du parcours utilisateur pour un accès fluide aux contenus audio.'
+    }
+  ];
+
+  constructor() {}
+}
